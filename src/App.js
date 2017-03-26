553bed1c159d89d2c42ac4053fdf077628e4d41f
mericks <mericks@users.noreply.github.com>
2017-03-26 11:11:17 -0700

    add Route '/forms' to view Forms in development

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Switch
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './components/home/Home';
+import Forms from './components/forms/Forms';
 
 class App extends Component {
   constructor(props){
@@ -25,10 +21,10 @@ class App extends Component {
   render() {
     return (
       <Router>
-
-        <Switch>
-          <Route exact path='/' render={(props)=> <Home {...props} signedIn={this.state.signedIn} onSignIn={this.onSignIn}/>} />
-        </Switch>
+          <Switch>
+            <Route exact path='/' render={(props)=> <Home {...props} signedIn={this.state.signedIn} onSignIn={this.onSignIn}/>} />
+            <Route exact path='/forms' render={(props) => <Forms {...props} />} />
+          </Switch>
       </Router>
     );
   }

cf12e20dcaeda0ce66c164e37717581861561311
cmd-kvn <kvn.m.wong@gmail.com>
2017-03-27 09:52:59 -0700

    adds messages for Recruiter/SeekerIntro and Welcome

--- src/components/home/RecruiterIntro.js
+++ src/components/home/RecruiterIntro.js
@@ -3,8 +3,13 @@ import React from 'react';
 const RecruiterIntro = () => {
   return (
     <div>
+      <h2>Recruiters and hiring managers</h2>
       <p>
-        Recruiter instructions here
+        Blaze through resumes of qualified job seekers at the click of a button. 
+        Set up an account, enter up to three jobs to select for, and click away. 
+        Move right to keep resumes for a job, left to filter them out. Go back to 
+        the profile page to see the resume collections for individual postings and 
+        contact the candidates for hire!
       </p>
       <button>button to sign up</button>
     </div>
--- src/components/home/Welcome.js
+++ src/components/home/Welcome.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Welcome = () => {
   return (
     <div>
-      <p>Welcome to the fastest way to sort through resumes and find a job. JobOrNot 
-        is built for recruiters, hiring managers, and job seekers with an easy to use 
-        interface to immediately get things done.
+      <p><strong>Welcome to the fastest way to sort through resumes and find a job. </strong>
+      JobOrNot is designed for recruiters, hiring managers, and job seekers with an easy to use 
+      interface to immediately get things done.
       </p>
     </div>
   )

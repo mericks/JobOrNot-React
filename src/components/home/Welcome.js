108ef072d27d12650e476a0eb4426a81058f645d
cmd-kvn <kvn.m.wong@gmail.com>
2017-03-27 09:14:21 -0700

    adds message to Welcome

--- src/components/home/Welcome.js
+++ src/components/home/Welcome.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Welcome = () => {
   return (
     <div>
-      <p>Welcome message from JobOrNot</p>
+      <p>Welcome to the fastest way to sort through resumes and find a job. JobOrNot 
+        is built for recruiters, hiring managers, and job seekers with an easy to use 
+        interface to immediately get things done.
+      </p>
     </div>
   )
 }

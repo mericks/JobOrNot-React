dd446a16f2ea7e08c22152a1539c15df1e9148b8
cmd-kvn <kvn.m.wong@gmail.com>
2017-03-28 18:03:32 -0700

    fixes temp img placeholder adds Nav into Profile

--- src/components/navbar/SignedInNav.js
+++ src/components/navbar/SignedInNav.js
@@ -21,7 +21,7 @@ const SignedInNav = (props) => {
     return (
       <nav>
         <ul>
-          <li><img src='http://placehold.it/50x50' alt='logo' /></li>
+          <li><img src='https://placeholdit.imgix.net/~text?txtsize=6&txt=50%C3%9750&w=50&h=50' alt='logo' /></li>
           <li>JobOrNot</li>
           <li>How It Works</li>
           <li>Profile</li>
--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import TalentProfile from './TalentProfile';
 import RecruiterProfile from './RecruiterProfile';
 import ProfileInfo from './ProfileInfo';
+import Nav from '../navbar/Nav';
 
 const mapStateToProps = (state) => {
     return {
@@ -18,11 +19,12 @@ const Profile = (props) => {
 
     return (
         <div>
+            <Nav />
             <h1>Hello {props.user.firstName}</h1>
             <ProfileInfo user={props.user} />
             <ProfileType />
         </div>
-    )
+    );
 };
 
 export default connect(mapStateToProps)(Profile);

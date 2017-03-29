bc75ee3e4b83f3269748032db2ad3d8306a630b7
mericks <mericks@users.noreply.github.com>
2017-03-29 16:59:35 -0700

    displays user stats in profile

--- src/components/profile/ProfileInfo.js
+++ src/components/profile/ProfileInfo.js
@@ -7,20 +7,36 @@ const mapStateToProps = (state) => {
     };
 };
 
-const ProfileFormFields = (props) => {
-    let obj = props.user;
-    for (const prop in obj) {
-        console.log('obj: ', prop, '=', obj[prop])
-        let formFields = obj[prop];
-        return <li>{formFields}</li>;
-    };
-};
+/* mapping an object, excluded from view */
+// const ProfileFormFields = (props) => {
+//     let obj = props.user;
+//     let listItemArray = [];
+//     for (const prop in obj) {
+//         console.log('obj: ', prop, '=', obj[prop]);
+//         let formFields = obj[prop];
+//         if (prop !== 'hash' && prop !== '__v' && prop !== '_id' && 
+//             prop !== 'jobTitleToFill' && prop !== 'jobCompanyToFill' && 
+//             prop !== 'role') {
+//             listItemArray.push(<li key={prop}>{prop}: {formFields}</li>);
+//         }
+//     };
+//     console.log('liarry is', listItemArray);
+//     return (<ul>{listItemArray}</ul>);
+// };
 
 const ProfileInfo = (props) => {
+    console.log('prosp.user: ', props.user);
+    let user = props.user
+    // let fields = ProfileFormFields();
+    // console.log('fields is', fields);
     return (
         <div>
-            <h3>where user object info should list out</h3>
-            <ul>{ProfileFormFields}</ul>
+            <h3>Profile</h3>
+            <p>Name: {user.firstName} {user.lastName}</p>
+            <p>Username: {user.username}</p>
+            <p>Email: {user.email}</p>
+            <p>Company: {user.company}</p>
+            <p>Favorited resumes: {user.likedResumes}</p>
         </div>
     );
 }; 

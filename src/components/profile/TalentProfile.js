9e38761b0855f853a8c7b699971188943111ae66
mericks <mericks@users.noreply.github.com>
2017-03-28 23:25:50 -0700

    add basic working skeleton of TalentProfile

--- src/components/profile/TalentProfile.js
+++ src/components/profile/TalentProfile.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+const mapStateToProps = (state) => {
+    return {
+        user: state.userAuth.user
+    };
+};
 
 const TalentProfile = (props) => {
-    
+    return (
+        <div>
+            <p>THIS IS TALENTPROFILE SECTION</p>
+            <p>username = {props.user.username}</p>
+        </div>
+    );
 }
 
-export default TalentProfile;
+export default connect(mapStateToProps)(TalentProfile);
+
+TalentProfile.propTypes = {
+    user: PropTypes.object
+};

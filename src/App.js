ef9e4c1276284b5ea4822350a7cf171fd6b9d9a3
mericks <mericks@users.noreply.github.com>
2017-03-28 23:06:32 -0700

    add /profile route for testing purposes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/home/Home';
 import RecruiterForm from './components/forms/RecruiterForm';
 import TalentForm from './components/forms/TalentForm';
 import SignInForm from './components/forms/SignInForm';
+import Profile from './components/profile/Profile';
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
           <Route exact path='/recruitersignup' component={RecruiterForm} />
           <Route exact path='/talentsignup' component={TalentForm} />
           <Route exact path='/signin' component={SignInForm} />
+          <Route exact path='/profile' component={Profile} />
         </div>
       </Router>
     );

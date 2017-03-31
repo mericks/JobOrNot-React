2ece606fd14b2c3acf3f78434ed0e4d03e8f8403
mericks <mericks@users.noreply.github.com>
2017-03-31 03:00:16 -0700

    adjust spacing on Home componenets

--- src/components/home/WelcomeBuffer.js
+++ src/components/home/WelcomeBuffer.js
@@ -4,6 +4,7 @@ const WelcomeBuffer = () => (
   <div className='container' id='welcomeBuffer'>
     <div className='row'>
         <div className='centerText'>
+            <br />
             <h4>The fastest way to sort through resumes <strong>and</strong> find a job.</h4>
             <h1 className='headline'>Seriously.</h1>
         </div>
